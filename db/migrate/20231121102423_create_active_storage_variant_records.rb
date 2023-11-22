815ac2cbb2fc1602baf5b5139767593ed537aedf
Benson Kiai <benson.kiai86@gmail.com>
2023-11-22 06:59:24 +0300

    Add Active Storage tables and foreign keys

---
+++ db/migrate/20231121102423_create_active_storage_variant_records.rb
@@ -0,0 +1,11 @@
+class CreateActiveStorageVariantRecords < ActiveRecord::Migration[7.1]
+  def change
+    create_table :active_storage_variant_records do |t|
+      t.bigint :blob_id, null: false
+      t.string :variation_digest, null: false
+
+      t.index [:blob_id, :variation_digest], name: "index_active_storage_variant_records_uniqueness", unique: true
+      t.timestamps
+    end
+  end
+end
---
+++ db/migrate/20231121125933_add_foreign_keys_to_active_storage.rb
@@ -0,0 +1,6 @@
+class AddForeignKeysToActiveStorage < ActiveRecord::Migration[7.1]
+  def change
+    add_foreign_key :active_storage_attachments, :active_storage_blobs, column: :blob_id
+    add_foreign_key :active_storage_variant_records, :active_storage_blobs, column: :blob_id
+  end
+end
