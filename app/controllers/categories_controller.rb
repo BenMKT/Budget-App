3105a49a0ac6329e58113e462ae0bb9fb6029906
Benson Kiai <benson.kiai86@gmail.com>
2023-11-22 06:58:34 +0300

    Add app controllers and actions

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,70 +1,49 @@
 class CategoriesController < ApplicationController
-  before_action :set_category, only: %i[ show edit update destroy ]
+  before_action :authenticate_user!
 
-  # GET /categories or /categories.json
   def index
-    @categories = Category.all
+    @categories = current_user.categories.includes(:transactions)
+    @total_amounts = {}
+
+    @categories.each do |category|
+      total_amount = category.transactions.sum(:amount)
+      @total_amounts[category.id] = total_amount
+    end
   end
 
-  # GET /categories/1 or /categories/1.json
   def show
+    @category = current_user.categories.find(params[:id])
+    @transactions = @category.transactions.order(created_at: :desc)
+    @total_amount = @transactions.sum(:amount)
   end
 
-  # GET /categories/new
   def new
-    @category = Category.new
+    @category = current_user.categories.build
   end
 
-  # GET /categories/1/edit
-  def edit
-  end
-
-  # POST /categories or /categories.json
   def create
-    @category = Category.new(category_params)
-
-    respond_to do |format|
-      if @category.save
-        format.html { redirect_to category_url(@category), notice: "Category was successfully created." }
-        format.json { render :show, status: :created, location: @category }
-      else
-        format.html { render :new, status: :unprocessable_entity }
-        format.json { render json: @category.errors, status: :unprocessable_entity }
-      end
+    @category = current_user.categories.build(category_params)
+    if @category.save
+      redirect_to categories_path, notice: "Category '#{@category.name}' was successfully created."
+    else
+      render :new
     end
   end
 
-  # PATCH/PUT /categories/1 or /categories/1.json
-  def update
-    respond_to do |format|
-      if @category.update(category_params)
-        format.html { redirect_to category_url(@category), notice: "Category was successfully updated." }
-        format.json { render :show, status: :ok, location: @category }
-      else
-        format.html { render :edit, status: :unprocessable_entity }
-        format.json { render json: @category.errors, status: :unprocessable_entity }
-      end
-    end
-  end
-
-  # DELETE /categories/1 or /categories/1.json
   def destroy
-    @category.destroy!
+    @category = Category.find(params[:id])
 
-    respond_to do |format|
-      format.html { redirect_to categories_url, notice: "Category was successfully destroyed." }
-      format.json { head :no_content }
+    if current_user == @category.user
+      @category.destroy
+      redirect_to categories_path, notice: 'Category was successfully deleted.'
+    else
+      redirect_to categories_path, alert: 'You are not authorized to delete this category.'
     end
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_category
-      @category = Category.find(params[:id])
-    end
 
-    # Only allow a list of trusted parameters through.
-    def category_params
-      params.require(:category).permit(:name, :icon, :user_id)
-    end
+  def category_params
+    params.require(:category).permit(:name, :icon)
+  end
 end
--- app/controllers/transactions_controller.rb
+++ app/controllers/transactions_controller.rb
@@ -1,70 +1,34 @@
 class TransactionsController < ApplicationController
-  before_action :set_transaction, only: %i[ show edit update destroy ]
+  before_action :authenticate_user!
+  before_action :find_category, only: %i[index new create]
 
-  # GET /transactions or /transactions.json
   def index
-    @transactions = Transaction.all
+    @transactions = @category.transactions.order(created_at: :desc)
+    @total_amount = @transactions.sum(:amount)
   end
 
-  # GET /transactions/1 or /transactions/1.json
-  def show
-  end
-
-  # GET /transactions/new
   def new
-    @transaction = Transaction.new
+    @transaction = @category.transactions.build
+    @categories = current_user.categories
   end
 
-  # GET /transactions/1/edit
-  def edit
-  end
-
-  # POST /transactions or /transactions.json
   def create
-    @transaction = Transaction.new(transaction_params)
-
-    respond_to do |format|
-      if @transaction.save
-        format.html { redirect_to transaction_url(@transaction), notice: "Transaction was successfully created." }
-        format.json { render :show, status: :created, location: @transaction }
-      else
-        format.html { render :new, status: :unprocessable_entity }
-        format.json { render json: @transaction.errors, status: :unprocessable_entity }
-      end
-    end
-  end
-
-  # PATCH/PUT /transactions/1 or /transactions/1.json
-  def update
-    respond_to do |format|
-      if @transaction.update(transaction_params)
-        format.html { redirect_to transaction_url(@transaction), notice: "Transaction was successfully updated." }
-        format.json { render :show, status: :ok, location: @transaction }
-      else
-        format.html { render :edit, status: :unprocessable_entity }
-        format.json { render json: @transaction.errors, status: :unprocessable_entity }
-      end
-    end
-  end
-
-  # DELETE /transactions/1 or /transactions/1.json
-  def destroy
-    @transaction.destroy!
-
-    respond_to do |format|
-      format.html { redirect_to transactions_url, notice: "Transaction was successfully destroyed." }
-      format.json { head :no_content }
+    @transaction = @category.transactions.build(transaction_params)
+    if @transaction.save
+      redirect_to category_transactions_path(@category), notice: 'Transaction was successfully created.'
+    else
+      @categories = current_user.categories
+      render :new
     end
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_transaction
-      @transaction = Transaction.find(params[:id])
-    end
 
-    # Only allow a list of trusted parameters through.
-    def transaction_params
-      params.require(:transaction).permit(:author_id, :name, :amount, :category_id)
-    end
+  def find_category
+    @category = current_user.categories.find(params[:category_id])
+  end
+
+  def transaction_params
+    params.require(:transaction).permit(:name, :amount, :category_id)
+  end
 end
